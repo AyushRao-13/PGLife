f435ee85e362851203dff895f3147b7476721755
AyushRao-13 <ayushyadav13701@gmail.com>
2023-10-22 12:43:32 +0530

    php

--- dashboard.php
+++ dashboard.php
@@ -163,16 +163,16 @@
         <div class="page-container footer-container">
             <div class="footer-cities">
                 <div class="footer-city">
-                    <a href="property_list.html">PG in Delhi</a>
+                    <a href="property_list.php">PG in Delhi</a>
                 </div>
                 <div class="footer-city">
-                    <a href="property_list.html">PG in Mumbai</a>
+                    <a href="property_list.php">PG in Mumbai</a>
                 </div>
                 <div class="footer-city">
-                    <a href="property_list.html">PG in Bangalore</a>
+                    <a href="property_list.php">PG in Bangalore</a>
                 </div>
                 <div class="footer-city">
-                    <a href="property_list.html">PG in Hyderabad</a>
+                    <a href="property_list.php>PG in Hyderabad</a>
                 </div>
             </div>
             <div class="footer-copyright">© 2020 Copyright PG Life </div>
